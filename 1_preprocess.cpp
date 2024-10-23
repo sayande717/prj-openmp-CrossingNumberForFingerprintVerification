aacccfb57bae3e8c4ec5dd3ef5e72479934d86a0
sayande717 <sayande717@github.com>
2024-10-23 19:14:51 +0530

    feat:implemented code & added assets

---
+++ 1_preprocess.cpp
@@ -0,0 +1,31 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace cv;
+
+void preprocessImage(const char* inputPath, const char* outputPath) {
+    // Load the image
+    Mat image = imread(inputPath, IMREAD_GRAYSCALE);
+    if (image.empty()) {
+        printf("Could not open or find the image\n");
+        return;
+    }
+
+    // Convert to binary image using thresholding
+    Mat binaryImage;
+    threshold(image, binaryImage, 128, 255, THRESH_BINARY);
+
+    // Save the binary image
+    imwrite(outputPath, binaryImage);
+}
+
+int main() {
+    const char* inputPath = "./assets/control.png";
+    const char* outputPath = "./assets/binary/test_1.png";
+
+    preprocessImage(inputPath, outputPath);
+
+    std::cout << "Preprocessing completed. Binary image saved to" << outputPath << std::endl;
+    return EXIT_SUCCESS;
+}
